69301c4f9a92860a98edbf6c5a56ab8d34f430d4
Philippe Vlérick <pvlerick@gmail.com>
2024-05-29 13:38:43 +0200

    ping

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,45 @@
-use std::net::TcpListener;
+use std::{
+    error::Error,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_request(&stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
+}
+
+fn handle_request(mut stream: &TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut buf = [0u8; 1024];
+    let bytes_read = stream.read(&mut buf)?;
+
+    if bytes_read > 0 {
+        match &buf[..bytes_read] {
+            b"PING" => return ping(stream),
+            _ => return Ok(()),
+        }
+    }
+
+    Ok(())
+}
+
+fn ping(mut stream: &TcpStream) -> Result<(), Box<dyn Error>> {
+    stream.write_all(b"+PONG\r\n")?;
+
+    Ok(())
 }
